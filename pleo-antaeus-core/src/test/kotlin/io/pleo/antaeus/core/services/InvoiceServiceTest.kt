920da1e3dec4ff56f6aa7b3584cef3a36bd8a361
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2021-02-20 00:27:02 +0100

    Setup test class for InvoiceService
    Added a mocked temporary database and a function `init` that initialises its state between each test

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -1,23 +1,56 @@
 package io.pleo.antaeus.core.services
 
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.impl.annotations.MockK
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
-import io.pleo.antaeus.data.AntaeusDal
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import io.pleo.antaeus.db.createDb
+import io.pleo.antaeus.models.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.math.BigDecimal
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class InvoiceServiceTest {
-    private val dal = mockk<AntaeusDal> {
-        every { fetchInvoice(404) } returns null
+
+    @MockK
+    private val db = createDb("test", false)
+
+    private val invoiceService = InvoiceService(dal = db)
+
+    @BeforeEach
+    fun init() {
+
+        invoiceService.deleteAll()
+
+        invoiceService
+            .create(
+                Money(BigDecimal(10), Currency.EUR),
+                Customer(0, Currency.EUR),
+                InvoiceStatus.PENDING
+            )
+
+        invoiceService
+            .create(
+                Money(BigDecimal(20), Currency.GBP),
+                Customer(1, Currency.GBP),
+                InvoiceStatus.PAID
+            )
     }
 
-    private val invoiceService = InvoiceService(dal = dal)
 
     @Test
     fun `will throw if invoice is not found`() {
         assertThrows<InvoiceNotFoundException> {
             invoiceService.fetch(404)
         }
+    }
+
+
+    @Test
+    fun `will throw if fetchByStatus fails`() {
+        val pending : List<Invoice> = invoiceService.fetchByStatus(InvoiceStatus.PENDING)
+        assertEquals(pending.size, 1)
+        assertEquals(pending[0].customerId, 0)
+        assertEquals(pending[0].status, InvoiceStatus.PENDING)
     }
 }
