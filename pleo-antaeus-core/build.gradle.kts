dde17824feabaa791d655408283d00cc468bfb21
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2021-02-19 12:31:22 +0100

    Added kotlinx.coroutines library to dependencies

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -6,5 +6,7 @@ kotlinProject()
 
 dependencies {
     implementation(project(":pleo-antaeus-data"))
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     api(project(":pleo-antaeus-models"))
 }
