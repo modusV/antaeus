c02838ea4c809850c6d9abf1bba2a1cbb3f4abb9
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2021-02-20 19:09:33 +0100

    Added function to charge customer in the billing service

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,15 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
 // TODO - Add code e.g. here
+
+    fun payInvoice(invoice : Invoice) : Boolean {
+        return paymentProvider.charge(invoice)
+    }
+
 }
