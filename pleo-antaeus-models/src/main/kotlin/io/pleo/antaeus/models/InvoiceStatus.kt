d281d3a8e4189b2cd29f27c6669554d12d1c5e83
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2021-02-20 19:14:03 +0100

    Added additional states to the invoice object
    Added states to understand whether the invoice is being paid, if it was payed successfully, or it failed due to some exception.

--- pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/InvoiceStatus.kt
+++ pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/InvoiceStatus.kt
@@ -2,5 +2,10 @@ package io.pleo.antaeus.models
 
 enum class InvoiceStatus {
     PENDING,
-    PAID
+    PAID,
+    PAYING,
+    FAILED_NETWORK,
+    FAILED_EMPTY_ACCOUNT,
+    FAILED_CUSTOMER_NOT_FOUND,
+    FAILED_CURRENCY_MISMATCH
 }
