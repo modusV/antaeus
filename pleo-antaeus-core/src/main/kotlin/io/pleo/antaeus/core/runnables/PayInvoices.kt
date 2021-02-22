3d630f936ff164810c730c1fd930b766fac0f777
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2021-02-22 01:53:44 +0100

    Added documentation to PayInvoices methods

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/runnables/PayInvoices.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/runnables/PayInvoices.kt
@@ -1,4 +1,4 @@
-package io.pleo.antaeus.core.recurring
+package io.pleo.antaeus.core.runnables
 
 import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
@@ -8,26 +8,19 @@ import io.pleo.antaeus.core.services.CustomerService
 import io.pleo.antaeus.core.services.InvoiceService
 import io.pleo.antaeus.models.*
 import kotlinx.coroutines.*
-import mu.KotlinLogging
-import java.lang.Runnable
-import java.math.BigDecimal
 
-private val logger = KotlinLogging.logger {}
 
-class PaymentScheduler (
+class PayInvoices (
         private val billingService : BillingService,
         private val customerService: CustomerService,
         private val invoiceService: InvoiceService
     ) {
 
-    /**
-    override fun run() {
-        // time perform Payments here
-        logger.debug { "Thread has run" }
-    }
-    */
 
-    fun payInvoice(invoice : Invoice) : Pair<Int, InvoiceStatus> {
+    /**
+     * Pay a single [invoice]. Returns a pair representing the invoice id and the billing outcome
+     */
+    private fun payInvoice(invoice : Invoice) : Pair<Int, InvoiceStatus> {
         try {
             invoiceService.updateStatusById(invoice.id, InvoiceStatus.PAYING)
             val res = billingService.payInvoice(invoice)
@@ -54,8 +47,13 @@ class PaymentScheduler (
     }
 
 
-    suspend fun performPayments() : List<Pair<Int, InvoiceStatus>> {
-        val toPay : List<Invoice> = invoiceService.fetchByStatus(InvoiceStatus.PENDING)
+    /**
+     * Use the BillingService to pay all the invoices that are due.
+     * In case some of them fail due to network errors, reschedule
+     * this function to run again in t time.
+     */
+    suspend fun performAllPaymentsByStatus(status : InvoiceStatus = InvoiceStatus.PENDING) : List<Pair<Int, InvoiceStatus>> {
+        val toPay : List<Invoice> = invoiceService.fetchByStatus(status)
         val scope = CoroutineScope(Dispatchers.IO)
 
         val jobs =
@@ -70,52 +68,4 @@ class PaymentScheduler (
         return results
     }
 
-
-    /**
-     * Use the BillingService to pay the invoices that are due.
-     * In case some of them fail due to network errors, reschedule
-     * this function to run again in t time.
-     */
-    suspend fun syncroTest(many: Int) {
-        val scope = CoroutineScope(Dispatchers.Unconfined)
-
-        val status =
-            (1..many)
-                .map {
-                    scope.async {
-                        try {
-                            val res = billingService.payInvoice(
-                                Invoice(
-                                    10, 1,
-                                    Money(BigDecimal.ONE, Currency.GBP),
-                                    InvoiceStatus.PENDING
-                                )
-                            )
-                            println("Result of $it is $res")
-                            0
-                        }
-                        catch (e : NetworkException) {
-                            println("Network Exception")
-                            1
-                        }
-                    }
-                }
-
-        println("${status.awaitAll().sum()} requests failed due to network problems")
-        println("All payed")
-        scope.cancel()
-    }
-
-    suspend fun test(v : Int) {
-        delay(1000L)
-        println("test $v completed")
-    }
-
-
-    init {
-        /*
-            Get time distance from here to next of the month and schedule
-            the performPayments function.
-        */
-    }
 }
